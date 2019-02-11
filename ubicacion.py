2f217407aa952e4597dcf93c0f75e8ccd4871bf3
rpenalozav <47232474+rpenalozav@users.noreply.github.com>
2019-02-10 19:21:49 -0500

    Create ubicacion.py

---
+++ ubicacion.py
@@ -0,0 +1,10 @@
+class ubicacion(object):
+	"""docstring for ubicacion"""
+	def __init__(self, indice, lapidaent, estado):
+		super(ubicacion, self).__init__()
+		self._indice = indice
+		self._lapidaent = lapidaent
+		self._estado = estado
+	def modificarUbicacion():
+		pass
+	
