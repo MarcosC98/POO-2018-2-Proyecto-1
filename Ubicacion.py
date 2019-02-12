1276f8da3ea78a20bce8d13a62c695457c8608fd
rpenalozav <47232474+rpenalozav@users.noreply.github.com>
2019-02-11 21:21:06 -0500

    Update Ubicacion.py

--- Ubicacion.py
+++ Ubicacion.py
@@ -1,10 +1,9 @@
 class Ubicacion():
 
 	def __init__(self, indice, lapidaent, estado):
-		super(ubicacion, self).__init__()
 		self._indice = indice
 		self._lapidaent = lapidaent
 		self._estado = estado
-	def modificarUbicacion():
-		pass
+	def modificarUbicacion(self, indice):
+		self._indice=indice
 	
