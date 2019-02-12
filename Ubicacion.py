58666f3ce80bfe445b84ea39c4c6f246a650e74f
MarcosC98 <47372077+MarcosC98@users.noreply.github.com>
2019-02-11 22:31:47 -0500

    Update Ubicacion.py

--- Ubicacion.py
+++ Ubicacion.py
@@ -1,10 +1,18 @@
-class Ubicacion():
+class ubicacion:
+	ubicaciones_disponibles=50
+	ubicaciones_totales = []
+	indices_usados = []
+	def __init__(self,indice,idCliente=0):
+		while indice is in indices_usados:
+			print("Está ubicación está ocupada, ingrese una ubicación distinta")
+			if indice is not in indices_usados:
+				indices_usados.append(indice)
+				self._indice = indice
+				self._idCliente = idCliente
+				break
 
-	def __init__(self, indice, lapidaent, estado):
-		self._indice = indice
-		self._lapidaent = lapidaent
-		self._estado = estado
-		
-	def modificarUbicacion(self, indice):
-		self._indice = indice
-	
+	def revisarDisponibilidadUbicacion(u):
+		if u is not in indices_usados:
+			return True
+		else:
+			return False	
