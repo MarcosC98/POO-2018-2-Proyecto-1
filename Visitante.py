916425256fbd8f4430acd3fe1affd599798dfda0
rpenalozav <47232474+rpenalozav@users.noreply.github.com>
2019-02-18 22:11:13 -0500

    Update Visitante.py

--- Visitante.py
+++ Visitante.py
@@ -1,6 +1,7 @@
 from Persona import Persona
 class Visitante(Persona):
-	visitantes_totales = []
+	visitantes_totales = 0
 
 	def __init__(self,nombre,fechaNac):
 		super().__init__(nombre,fechaNac)
+		Visitante.visitantes_totales = visitantes_totales+1
