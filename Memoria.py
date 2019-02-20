c5a902d6bcb4e67dba94ad097514136293de23f1
rpenalozav <47232474+rpenalozav@users.noreply.github.com>
2019-02-20 18:48:04 -0500

    Update Memoria.py

--- Memoria.py
+++ Memoria.py
@@ -4,14 +4,14 @@ class Memoria:
 	id = 0
 	
 	def __init__(self,autor,descripcion,lapida):
-		self.autor = autor
+		self._autor = autor
 		self._descripcion = descripcion
-		self.lapida = lapida
+		self._lapida = lapida
 		self._fechaPub =  str(datetime.datetime.now())
 
 	def imprimirDatosMemoria(self):
-		print("Memoria de " + self.lapida._persona.getNombre())
+		print("Memoria de " + self._lapida._persona.getNombre())
 		print(self._descripcion)
-		print("Escrito en : " + self._fechaPub + " por " + self.autor.getNombre())
+		print("Escrito en : " + self._fechaPub + " por " + self._autor.getNombre())
 		print("-------------------------------------------------------------------------------------------------------------------------------------------------------")
 		
