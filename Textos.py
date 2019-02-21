16afcc8c19b7fbb6acb93cd89ba463ccc76071f0
CarlosMarioCA <camchavezag@unal.edu.co>
2019-02-20 21:11:19 -0500

    Textos 1

--- Textos.py
+++ Textos.py
@@ -46,7 +46,7 @@ class Texto:
 			self.ingresoEpitafio = ""+"\n"+"Ingrese el epitafio que desea en su lapida: "
 			self.ingresoContrasena = ""+"\n"+"Ingrese la contraseña que usará en su cuenta: "
 			self.ingresoUbicacion = ""+"\n"+"Ingrese la úbicación de su lápida: "
-			self.selecPrivacidad = ""+"\n"+"Ingrese 0 si desea que su lápida sea privada"+"\n"+"Ingrese 1 si desea que sea pública "
+			self.selecPrivacidad = ""+"\n"+"Ingrese 0 si desea que su lápida sea privada"+"\n"+"Ingrese 1 si desea que sea pública (Recomendado) "
 			self.lapidaOcupada = ""+"\n"+"Esta ubicación ya se encuentra ocupada, selecciona una distinta por favor."
 			self.lapidaAntesCreada = ""+"\n"+"Ya tienes una lapida creada."
 			self.propietarioLap = ""+"\n"+"Ingrese el documento de la persona propietaria de la lapida donde va a dejar la memoria: "
@@ -55,14 +55,6 @@ class Texto:
 			self.lapidaInvalida = ""+"\n"+"No hay ninguna persona con ese documento que tenga una lapida."
 			self.ingresoPropietario = ""+"\n"+"Ingrese el documento de la persona propietaria de la lapida que desea leer: "
 			self.noLapida = ""+"\n"+"No hay ninguna persona con ese documento que tenga una lapida."
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
-			self.metodo = ""+"\n"+""
 
 			
 		else:
@@ -71,8 +63,20 @@ class Texto:
 			self.noRegistra = ""+"\n"+"There isn't any registered account with that id number."
 			self.seleccionUno = ""+"\n"+"If you want to try again with another document, enter 0."+"\n"+"If you want to create a new account with this document, enter 1."+"\n"+""
 			self.entradaNombre = ""+"\n"+"Enter your complete name: "
-			self.entradaFechaNac = ""+"\n"+""
-			self.metodo = ""+"\n"+""
+			self.entradaFechaNac = ""+"\n"+"Enter your Birth day: "
+			self.selecOpcion = ""+"\n"+"Choose one of the follow options: "+"\n"+" "+"\n"+"  1.Purchase a tombstone."+"\n"+"  2.Write Memory "+"\n"+"  3.Read tombstone "+"\n"+"  4.Enter with another profile "+"\n"+"  5.Game Over "
+			self.ingresoEpitafio = ""+"\n"+"Write the sentence of your Epitaph: "
+			self.ingresoContrasena = ""+"\n"+"Enter the password of your account: "
+			self.ingresoUbicacion = ""+"\n"+"Enter the location of your tombstone: "
+			self.selecPrivacidad = ""+"\n"+"Type 0 if you want your tombstone to be private"+"\n"+"Type 1 if you want a public tombstone (Recommended) "
+			self.lapidaOcupada = ""+"\n"+"This location is already occupied, select a different one please."
+			self.lapidaAntesCreada = ""+"\n"+"You already have a tombstone created."
+			self.propietarioLap = ""+"\n"+"Enter the document of the person who owns the tombstone where you will leave the memory: "
+			self.lapidaPrivada = ""+"\n"+"This tombstone is private. You can not read / write memories."
+			self.ingMemoria = ""+"\n"+"Enter the description of the memory please: "
+			self.lapidaInvalida = ""+"\n"+"There is no person with that document who has a tombstone."
+			self.ingresoPropietario = ""+"\n"+"Enter the document of the person who owns the tombstone you want to read: "
+			self.noLapida = ""+"\n"+"There is no person with that document who has a tombstone."
 
 
 
--- Ubicacion.py
+++ Ubicacion.py
@@ -1,4 +1,5 @@
 class Ubicacion:
+	
 	ubicaciones_disponibles=50
 	ubicaciones_totales = []
 	indices_usados = []
